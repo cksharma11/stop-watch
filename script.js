aaaed81f981302acf8aa5e20911e6b37623971a0
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-01-10 00:29:07 +0530

    Code Refactor
    moved global timeCounter inside function

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const hoursDiv = document.getElementById('hours');
 const minutesDiv = document.getElementById('minutes');
 const secondsDiv = document.getElementById('seconds');
-let totalSeconds = 0;
+
+const getSecondsFromTime = (hours, minutes, seconds) =>
+  hours * 60 * 60 + minutes * 60 + seconds;
+
+const getHoursCount = seconds => Math.floor(seconds / 60 / 60);
+const getMinutesCount = seconds => Math.floor((seconds / 60) % 60);
+const getSecondsCount = time => time % 60;
 
 const getTime = function() {
   const hours = +hoursDiv.value;
   const minutes = +minutesDiv.value;
   const seconds = +secondsDiv.value;
-  return hours * 60 * 60 + minutes * 60 + seconds;
+  return getSecondsFromTime(hours, minutes, seconds);
 };
 
 const getRemainingTime = function(time) {
-  let hours = Math.floor(time / 60 / 60);
-  let minutes = Math.floor((time / 60) % 60);
-  let seconds = time % 60;
+  const hours = getHoursCount(time);
+  const minutes = getMinutesCount(time);
+  const seconds = getSecondsCount(time);
+
   return { hours, minutes, seconds };
 };
 
@@ -29,18 +36,16 @@ const disableInputBoxes = function() {
   secondsDiv.disabled = 'disabled';
 };
 
-const startCountDown = function() {
+const startCountDown = function(totalSeconds) {
   let timer = setInterval(() => {
-    if (totalSeconds == 0) {
-      clearInterval(timer);
-    }
+    if (totalSeconds == 0) clearInterval(timer);
     let remainingTime = getRemainingTime(totalSeconds--);
     displayRemainingTime(remainingTime);
   }, 1000);
 };
 
 const startTimer = function() {
-  totalSeconds = getTime();
+  const totalSeconds = getTime();
   disableInputBoxes();
-  startCountDown();
+  startCountDown(totalSeconds);
 };
